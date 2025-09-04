df68e33116bec922de6c75046c7e42b66d4737bd
lee02g29 <lee02g29@naver.com>
2025-09-04 16:24:44 +0900

    Chore: 파이어베이스 세팅 설정 및 테스트 환경 설정

---
+++ yum-yum/src/services/firebase.js
@@ -0,0 +1,31 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import {
+  initializeAuth,
+  browserSessionPersistence,
+  indexedDBLocalPersistence,
+  browserLocalPersistence,
+} from "firebase/auth";
+
+// firebase 설정
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const firestore = getFirestore(app);
+
+// authentication 설정
+export const auth = initializeAuth(app, {
+  persistence: [
+    indexedDBLocalPersistence, // 브라우저가 종료되어도 유지, 모바일에서 더 안정적
+    browserLocalPersistence, // 브라우저가 종료되어도 유지, 호환성
+  ],
+});
