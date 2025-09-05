793a9a7b8089592a278e3ba1cc5510f0a91ae350
kimhana415 <khnaa415@gmail.com>
2025-09-05 11:26:35 +0900

    [feat] 헤더 컴포넌트 추가

--- yum-yum/src/components/Header.jsx
+++ yum-yum/src/components/Header.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+// 아이콘
+import logo from '/logo.svg';
+import PrevIcon from '@/assets/icons/icon-left.svg?react';
 
 export default function Header() {
+  const navigate = useNavigate();
+  const isHeaderHiddenPage = location.pathname.startsWith('/meal'); // 헤더 없는 페이지
+  const isBackHiddenPage = ['/', '/report', '/mypage'].includes(location.pathname); // e뒤로가기 버튼 없는 페이지
+
+  // 뒤로가기
+  const handleBack = () => {
+    navigate(-1);
+  };
+
   return (
-    <div>
-      <h1>Header: 이부분은 지워주세요</h1>
-    </div>
+    <>
+      {!isHeaderHiddenPage && (
+        <div className='flex items-center h-[60px] px-5'>
+          {!isBackHiddenPage && (
+            <button onClick={handleBack} className='flex items-center justify-center'>
+              <PrevIcon className='h-[60px] mr-4' />
+            </button>
+          )}
+
+          <h1>
+            <Link to='/' className='flex items-center w-[84px] h-[60px]'>
+              <img src={logo} alt='오늘의 냠냠' className='w-full' />
+            </Link>
+          </h1>
+        </div>
+      )}
+    </>
   );
 }
