e3771b99bb8f8c73838603db872fd0d40c846df9
Ahhhhhyeong <dkdud081@gmail.com>
2025-09-08 11:08:26 +0900

    [feat] 메인 화면 날짜 선택 헤더, 온보딩 추가

---
+++ yum-yum/src/components/common/OnBoarding.jsx
@@ -0,0 +1,23 @@
+// 온보드 화면(전체화면)
+import React from 'react';
+import BasicButton from '@/components/button/BasicButton';
+
+export default function OnBoarding({ isOpen = false, onClose }) {
+  if (!isOpen) return null;
+  return (
+    <div className='fixed inset-0 z-50 bg-white'>
+      <div className='w-full h-full overflow-auto flex items-start justify-center'>
+        <div className='relative max-w-full'>
+          {/* 전체화면을 채우는 이미지  */}
+          <img src='onboarding.png' alt='온보딩 이미지' className='max-w-full h-auto' />
+          {/* 닫기 버튼 */}
+          <div className='absolute top-4 right-4 z-10 bg-white'>
+            <BasicButton size='md' variant='line' onClick={onClose}>
+              Skip
+            </BasicButton>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
