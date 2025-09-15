03f529176dcddeddb82e57dee657d2e8e1f01b11
Ahhhhhyeong <dkdud081@gmail.com>
2025-09-15 09:59:50 +0900

    [fix] 소수점 첫째자리까지 표출

--- yum-yum/src/pages/home/card/calorie/CalorieNutrition.jsx
+++ yum-yum/src/pages/home/card/calorie/CalorieNutrition.jsx
@@ -5,15 +5,15 @@ export default function CalorieNutrition({ carbs = 0, protein = 0, fat = 0 }) {
     <div className='flex flex-row gap-8 item-center justify-center mt-[-30px] mb-4'>
       <div className='flex gap-1 item-center justify-center'>
         <div className='text-gray-800 text-sm font-normal'>탄</div>
-        <div className='text-gray-800 text-sm font-bold'>{carbs}g</div>
+        <div className='text-gray-800 text-sm font-bold'>{carbs.toFixed(1)}g</div>
       </div>
       <div className='flex gap-1 item-center justify-center'>
         <div className='text-gray-800 text-sm font-normal'>단</div>
-        <div className='text-gray-800 text-sm font-bold'>{protein}g</div>
+        <div className='text-gray-800 text-sm font-bold'>{protein.toFixed(1)}g</div>
       </div>
       <div className='flex gap-1 item-center justify-center'>
         <div className='text-gray-800 text-sm font-normal'>지</div>
-        <div className='text-gray-800 text-sm font-bold'>{fat}g</div>
+        <div className='text-gray-800 text-sm font-bold'>{fat.toFixed(1)}g</div>
       </div>
     </div>
   );
