4351e29300c08c4c0b430823194aa78eeadd3016
Ahhhhhyeong <dkdud081@gmail.com>
2025-09-16 10:09:49 +0900

    [feat] 로그인 하지 않은 유저의 화면

--- yum-yum/src/hooks/useAuth.js
+++ yum-yum/src/hooks/useAuth.js
@@ -2,7 +2,7 @@
 import { useState } from 'react';
 
 export default function useAuth() {
-  const [isAuthenticated, setIsAuthenticated] = useState(true); // 초기값을 true로 설정 (임시)
+  const [isAuthenticated, setIsAuthenticated] = useState(false); // 초기값을 true로 설정 (임시)
 
   // 이곳에서 쿠키를 확인하여 로그인 상태를 업데이트
 
--- yum-yum/src/pages/home/card/calorie/CalorieHeader.jsx
+++ yum-yum/src/pages/home/card/calorie/CalorieHeader.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import BasicButton from '@/components/button/BasicButton';
+import { useNavigate } from 'react-router-dom';
 
 export default function CalorieHeader() {
+  const navigate = useNavigate();
   return (
     <div className='flex justify-between items-center mb-6 '>
       <h2 className='text-gray-800 text-2xl font-extrabold'>오늘의 칼로리</h2>
-      <BasicButton size='xs' variant='line' onClick={() => {}}>
+      <BasicButton
+        size='xs'
+        variant='line'
+        onClick={() => {
+          navigate('/report', { replace: true });
+        }}
+      >
         전체보기
       </BasicButton>
     </div>
