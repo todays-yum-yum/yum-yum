b2255a6782f59208f00087d4bf48d633df2a99a3
Ahhhhhyeong <dkdud081@gmail.com>
2025-09-17 16:20:35 +0900

    [fix] staleTime 1분으로 되돌려놓음

--- yum-yum/src/hooks/useMainPageData.js
+++ yum-yum/src/hooks/useMainPageData.js
@@ -18,9 +18,8 @@ export const usePageData = (userId, selectedDate) => {
     queryKey: ['dailyData', userId, selectedDate],
     queryFn: () => getDailyData(userId, selectedDate),
     select: (resp) => resp.data,
-    // staleTime: 1 * 60 * 1000, // 1분
+    staleTime: 1 * 60 * 1000, // 1분
     // staleTime: 30 * 1000, // 30초
-    staleTime: 0, // 실시간(임시)
     enabled: !!userId && !!selectedDate,
   });
 
