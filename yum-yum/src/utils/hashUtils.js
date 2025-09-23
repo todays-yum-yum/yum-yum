73cf2a706c9815a00cbeff6653b170671bde5954
Ahhhhhyeong <dkdud081@gmail.com>
2025-09-23 13:25:22 +0900

    [fix] AI api 호출을 위한 hash 수정, meals데이터가 없을 때 조건문 수정

--- yum-yum/src/utils/hashUtils.js
+++ yum-yum/src/utils/hashUtils.js
@@ -1,8 +1,9 @@
 // 데이터 해시 생성
 export function generateDataHash(meals) {
-  const dataString = JSON.stringify(meals?.type);
-  const encoder = new TextEncoder();
-  const data = encoder.encode(dataString);
+  // console.log(meals);
+  // meals 객체의 totalNutrition 속성을 문자열로 변환하여 해시 생성
+  const dataString = JSON.stringify(meals?.totalNutrition);
+  const data = new TextEncoder().encode(dataString);
   let hash = 0;
   for (let i = 0; i < data.length; i++) {
     hash = (hash << 5) - hash + data[i];
