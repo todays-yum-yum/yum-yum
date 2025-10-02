970e027aae6c2f5de6dbdcc836b098b6a421e187
kimhana415 <khnaa415@gmail.com>
2025-10-02 15:35:58 +0900

    [refactor] aria-label 추가

--- yum-yum/src/pages/water/component/WaterSettings.jsx
+++ yum-yum/src/pages/water/component/WaterSettings.jsx
@@ -7,7 +7,11 @@ export default function WaterSettings({ oneTimeIntake, targetIntake, onOpenModal
     <div className='flex flex-col gap-[20px] px-[28px] py-[24px] bg-gray-50 rounded-2xl'>
       <div className='flex justify-between'>
         <h5 className='font-bold text-lg'>수분 섭취량</h5>
-        <button onClick={onOpenModal} className='flex items-center justify-center'>
+        <button
+          aria-label='수분 섭취량 설정 열기'
+          onClick={onOpenModal}
+          className='flex items-center justify-center'
+        >
           <SettingIcon />
         </button>
       </div>
@@ -15,11 +19,11 @@ export default function WaterSettings({ oneTimeIntake, targetIntake, onOpenModal
       <div className='flex flex-col gap-[8px]'>
         <div className='flex justify-between'>
           <p className='text-gray-700'>1회 섭취량</p>
-          <p className='font-extrabold'>{oneTimeIntake}ml</p>
+          <p className='font-extrabold'>{oneTimeIntake.toLocaleString()}ml</p>
         </div>
         <div className='flex justify-between'>
           <p className='text-gray-700'>목표 섭취량</p>
-          <p className='font-extrabold'>{targetIntake}ml</p>
+          <p className='font-extrabold'>{targetIntake.toLocaleString()}ml</p>
         </div>
       </div>
     </div>
